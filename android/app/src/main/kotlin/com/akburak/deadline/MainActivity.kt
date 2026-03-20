fa468364913d2adc205aa0ed010f100ed6ebe58d
hkursatakburak <akburakkursat54@gmail.com>
2026-03-20 16:05:32 +0300

    Chore: Update package name to com.akburak.deadline and update app icon

---
+++ android/app/src/main/kotlin/com/akburak/deadline/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.akburak.deadline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/deadlineapp/deadline_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.deadlineapp.deadline_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
